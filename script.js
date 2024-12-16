304193ad88426421ab409419a795bd27b36c166d
Deanna Mandarino <mandarinodeanna@gmail.com>
2024-12-16 13:34:41 -0500

    Added comments and cleaned up code

--- script.js
+++ script.js
@@ -1,49 +1,56 @@
+// Define the URL endpoint for fetching the city data (JSON format)
 const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
-
 const cities = [];
 
+// Fetch the city data from the endpoint and populate the cities array
 fetch(endpoint)
-  .then(blob => blob.json())
-  .then(data => cities.push(...data));
+  .then(response => response.json()) // Parse the JSON response
+  .then(data => cities.push(...data)); // Push the data into the cities array
 
+// Function to find matches in the cities array based on the input word
 function findMatches(wordToMatch, cities) {
-  // Escape special characters in the wordToMatch for a valid RegExp pattern
-  const regex = new RegExp(wordToMatch.replace(/[.*+?^=!:${}()|\[\]\/\\]/g, "\\$&"), 'gi');
+  const regex = new RegExp(wordToMatch, 'gi');
 
-  return cities.filter(place => {
-    return place.city.match(regex) || place.state.match(regex);
-  })
+  return cities.filter(place => place.city.match(regex) || place.state.match(regex));
 }
 
+// Function to format numbers with commas (e.g., 1000000 becomes 1.000.000)
 function numberWithCommas(x) {
-  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.');
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.'); // Adds a dot every 3 digits
 }
 
+// Function to display matching cities based on the input
 function displayMatches() {
   const matchArray = findMatches(this.value, cities);
 
   // If input is empty, clear the matches
   if (!this.value) {
-    matches.innerHTML = '';
+    matchesList.innerHTML = '';
     return;
   }
 
+  // Map over the matched cities and generate the HTML for the list
   const html = matchArray.map(place => {
     const regex = new RegExp(this.value, 'gi');
+
+    // Replace the matched city and state names with highlighted text
     const cityName = place.city.replace(regex, `<span class="highlight">${this.value}</span>`);
     const stateName = place.state.replace(regex, `<span class="highlight">${this.value}</span>`);
+
     return `
     <li class="search-form__match">
       <span class="name">${cityName}, ${stateName}</span>
       <span class="population">${numberWithCommas(place.population)}</span>
     </li>`;
   }).join('');
-  
-  matches.innerHTML = html;
+
+  // Inject the generated HTML into the matches list container
+  matchesList.innerHTML = html;
 }
 
+// Get references to the input field and the matches list container
 const searchInput = document.querySelector('.search-form__input');
-const matches = document.querySelector('.search-form__matches');
+const matchesList = document.querySelector('.search-form__matches');
 
-searchInput.addEventListener('change', displayMatches);
-searchInput.addEventListener('keyup', displayMatches);
+// Add an event listener to the search input field to trigger the displayMatches function when the user types
+searchInput.addEventListener('input', displayMatches);
